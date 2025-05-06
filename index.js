e612a77b98a946e67a028331d039b771257d1de2
Florian BRUN <flo33.brun@hotmail.fr>
2025-05-06 11:32:18 +0200

    Setting up the filter

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
-
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
-
-// 3 - Passer les données à une variable
-
-// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
-
-// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-// coutry.name.includes(inputSearch.value);
-
-// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
-
-// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
-
 let countryArray = [];
 
 let fetchCountries = async () => {
@@ -21,27 +6,26 @@ let fetchCountries = async () => {
     .then((data) => (countryArray = data));
   console.log(countryArray);
 
-  displayCountry();
+  displayCountry(countryArray);
 };
 
 fetchCountries();
 
-let displayCountry = () => {
-  // On boucle sur chaque pays de notre tableau countryArray
-  countryArray.forEach((country) => {
+let displayCountry = (countries) => {
+  let countryContainer = document.querySelector(".countries-container");
+  countryContainer.innerHTML = "";
+
+  countries.forEach((country) => {
     const displayCountryFlag = country.flags.png;
     const displayCountryName = country.name.common;
     const displayCountryCapital =
       country.capital && country.capital[0] ? country.capital[0] : "Inconnue";
     const displayCountryPopulation = country.population;
 
-    let countryContainer = document.querySelector(".countries-container");
-
     let countryCard = document.createElement("div");
-    countryCard.classList.add("country-card"); //
+    countryCard.classList.add("country-card");
     countryContainer.appendChild(countryCard);
 
-    // Injection des données dans la carte
     countryCard.innerHTML = `
       <img src="${displayCountryFlag}" alt="Flag ${displayCountryName}">
       <h1>Country: ${displayCountryName}</h1>
@@ -65,4 +49,6 @@ let searchFilter = (inputText) => {
     country.name.common.toLowerCase().includes(inputText.toLowerCase())
   );
   console.log(filteredCountries);
+
+  displayCountry(filteredCountries);
 };
